c067d65410bad13afca1a5b93f37d1f34c922357
sasha <s0679633388@gmail.com>
2021-02-26 10:11:13 +0200

    phones

---
+++ db_utils.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+try:
+    connection = sqlite3.connect('./db.sqlite3')
+
+    cursor = connection.cursor()
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS users
+              (id INTEGER NOT NULL PRIMARY KEY , first_name varchar(50), last_name varchar(50),
+               is_student BOOLEAN); ''')
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS phones
+                  (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT , value VARCHAR(50),
+                   user_id INTEGER, FOREIGN KEY(user_id) REFERENCES users(id)); ''')
+
+    connection.commit()
+finally:
+    connection.close()
--- main.py
+++ main.py
@@ -1,8 +1,165 @@
-def add(x, y):
-     return x + y
+from flask import Flask, request
+from utils import encrypt_message
+from utils import decrypt_message
+import string
+import random
+import requests
+import sqlite3
+
+app = Flask(__name__)
 
 
-def diff(x, y):
-     return x- y
+def generate_password(length: int = 10) -> str:
+    choices = string.ascii_letters
+    password = ''
+
+    for i in range(length):
+        password += random.choice(choices)
+
+    return password
 
 
+@app.route('/')
+def hello_world():
+    length = int (request.args.get('length') or 10)
+    name = request.args.get('name', 'DEFAULT')
+    age = request.args.get('age', 'DEFAULT_AGE')
+    return  f'{name} {age}, here is your password {generate_password(length)}'
+
+@app.route('/exp/')
+def exp():
+    return "exp"
+
+
+#@app.route('/encrypt-message/')
+#def encrypt_message_route():
+#    message = request.args['message']
+ #   return encrypt_message(message)
+
+
+#@app.route('/fizz-buzz/')
+#def fizz_buzz():
+ #   num = request.args['num']
+#    return fizz_buzz(num)
+
+
+#@app.route('/decrypt-message/')
+#def decrypt_message_route():
+#    message = request.args['message']
+ #   return decrypt_message(message)
+
+
+
+
+#@app.route('/space/')
+#def space():
+#    response = requests.get('http://api.open-notify.org/astros.json')
+#    num = response.json()['number']
+  #  return str(num)
+
+
+@app.route('/users/list/')
+def users():
+    import sqlite3
+
+    try:
+        connection = sqlite3.connect('./db.sqlite3')
+        cursor = connection.cursor()
+
+        cursor.execute("SELECT * FROM users;")
+        result = cursor.fetchall()
+
+        connection.commit()
+    finally:
+        connection.close()
+
+    return str(result)
+
+
+@app.route('/users/create/')
+def users_create():
+    import sqlite3
+
+    first_name = request.args['firstName']
+    last_name = request.args['lastName']
+    is_student = int(request.args['isStudent'] == 'true')
+    ID = random.randint(1, 100_000)
+
+    try:
+        connection = sqlite3.connect('./db.sqlite3')
+        cursor = connection.cursor()
+
+        query = f"INSERT INTO users VALUES ({ID}, '{first_name}', '{last_name}', {is_student});"
+        print(query)
+        cursor.execute(query)
+        connection.commit()
+    finally:
+        connection.close()
+
+    return "OK"
+
+
+@app.route('/phones/list/')
+def phones():
+    import sqlite3
+
+    try:
+        connection = sqlite3.connect('./db.sqlite3')
+        cursor = connection.cursor()
+
+        cursor.execute("SELECT * FROM phones;")
+        result = cursor.fetchall()
+
+        connection.commit()
+    finally:
+        connection.close()
+
+    return str(result)
+
+
+@app.route('/phones/create/')
+def phones_create():
+    import sqlite3
+
+    phone_value = request.args['phoneValue']
+    user_id = request.args['userId']
+
+    try:
+        connection = sqlite3.connect('./db.sqlite3')
+        cursor = connection.cursor()
+
+        query = f"INSERT INTO phones VALUES (null, '{phone_value}', {user_id});"
+        print(query)
+        cursor.execute(query)
+        connection.commit()
+    finally:
+        connection.close()
+
+    return "OK"
+
+
+@app.route('/users/phones/')
+def users_phones():
+
+    try:
+        connection = sqlite3.connect('./db.sqlite3')
+        cursor = connection.cursor()
+
+        query = f"""
+        SELECT phones.id, phones.value, users.first_name,users.last_name
+        FROM phones
+        INNER JOIN users ON phones.user_id = users_id;
+        """
+        cursor.execute(query)
+        result = cursor.fetchall()
+
+        connection.commit()
+    finally:
+        connection.close()
+
+    return str(result)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
